ae5dfa94e05ab3a4372be5e5eae7c22aa691d190
Keisuke Miyako <keisuke.miyako@4d.com>
2021-04-02 09:07:34 +0900

    apple script

--- gs/include/gserrors.h
+++ gs/include/gserrors.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2001-2012 Artifex Software, Inc.
+/* Copyright (C) 2001-2020 Artifex Software, Inc.
    All Rights Reserved.
 
    This software is provided AS-IS with no warranty, either express or
@@ -9,8 +9,8 @@
    of the license contained in the file LICENSE in this distribution.
 
    Refer to licensing information at http://www.artifex.com or contact
-   Artifex Software, Inc.,  7 Mt. Lassen Drive - Suite A-134, San Rafael,
-   CA  94903, U.S.A., +1(415)492-9861, for further information.
+   Artifex Software, Inc.,  1305 Grant Avenue - Suite 200, Novato,
+   CA 94945, U.S.A., +1(415)492-9861, for further information.
 */
 
 
@@ -100,6 +100,12 @@ enum gs_error_type {
  * Internal code for requesting more input from run_string.
  */
     gs_error_NeedInput = -106,
+
+/*
+ * Internal code to all run_string to request that the data is rerun
+ * using run_file.
+ */
+    gs_error_NeedFile = -107,
 
 /*
  * Internal code for a normal exit when usage info is displayed.
